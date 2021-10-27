2ec46149b90b866f6b11a89cfce9ec043d3a98f9
YY <yanyu@tri-japan.com>
2021-10-27 19:46:23 +0900

    feat: add locale jaJP

---
+++ packages/web-vue/components/locale/lang/ja-jp.ts
@@ -0,0 +1,150 @@
+import { ArcoCalendarLang, ArcoLang } from '../interface';
+
+const calendarLang: ArcoCalendarLang = {
+  formatYear: 'YYYY 年',
+  formatMonth: 'YYYY 年 MM 月',
+  today: '今日',
+  view: {
+    month: '月',
+    year: '年',
+    week: '週',
+    day: '日',
+  },
+  month: {
+    long: {
+      January: '一月',
+      February: '二月',
+      March: '三月',
+      April: '四月',
+      May: '五月',
+      June: '六月',
+      July: '七月',
+      August: '八月',
+      September: '九月',
+      October: '十月',
+      November: '十一月',
+      December: '十二月',
+    },
+    short: {
+      January: '一月',
+      February: '二月',
+      March: '三月',
+      April: '四月',
+      May: '五月',
+      June: '六月',
+      July: '七月',
+      August: '八月',
+      September: '九月',
+      October: '十月',
+      November: '十一月',
+      December: '十二月',
+    },
+  },
+  week: {
+    long: {
+      self: '週',
+      monday: '月曜日',
+      tuesday: '火曜日',
+      wednesday: '水曜日',
+      thursday: '木曜日',
+      friday: '金曜日',
+      saturday: '土曜日',
+      sunday: '日曜日',
+    },
+    short: {
+      self: '週',
+      monday: '月',
+      tuesday: '火',
+      wednesday: '水',
+      thursday: '木',
+      friday: '金',
+      saturday: '土',
+      sunday: '日',
+    },
+  },
+};
+
+const lang: ArcoLang = {
+  locale: 'ja-JP',
+  empty: {
+    description: 'データなし',
+  },
+  drawer: {
+    okText: 'OK',
+    cancelText: 'キャンセル',
+  },
+  popconfirm: {
+    okText: 'OK',
+    cancelText: 'キャンセル',
+  },
+  modal: {
+    okText: 'OK',
+    cancelText: 'キャンセル',
+  },
+  pagination: {
+    goto: '',
+    page: 'ページ',
+    countPerPage: '件/ページ',
+    total: '総計 {0} 件',
+  },
+  table: {
+    okText: 'OK',
+    resetText: 'リセット',
+  },
+  upload: {
+    start: 'スタート',
+    cancel: 'キャンセル',
+    delete: '削除',
+    retry: 'リトライ',
+    buttonText: 'アップロード',
+    preview: 'プレビュー',
+    drag: 'クリックまたはドラッグしてアップロードします。',
+    dragHover: 'ドロップしてアップロードを始めます',
+    error: 'アップロード失敗しました',
+  },
+  datePicker: {
+    view: calendarLang.view,
+    month: calendarLang.month,
+    week: calendarLang.week,
+    placeholder: {
+      date: '日付を選択',
+      week: '週を選択',
+      month: '月を選択',
+      year: '年を選択',
+      quarter: '四半期を選択',
+      time: '時間を選択',
+    },
+    rangePlaceholder: {
+      date: ['開始日', '終了日'],
+      week: ['開始週', '終了週'],
+      month: ['開始月', '終了月'],
+      year: ['開始年', '終了年'],
+      quarter: ['開始四半期', '終了四半期'],
+      time: ['開始時間', '終了時間'],
+    },
+    selectTime: '時間を選択',
+    today: '今日',
+    now: '今',
+    ok: 'OK',
+  },
+  image: {
+    loading: 'Loading',
+  },
+  imagePreview: {
+    fullScreen: 'フルスクリーン',
+    rotateRight: '右回り',
+    rotateLeft: '左回り',
+    zoomIn: '拡大',
+    zoomOut: '縮小',
+    originalSize: 'デフォルト',
+  },
+  typography: {
+    copied: 'コピーしました',
+    copy: 'コピー',
+    expand: '展開',
+    collapse: '折る',
+    edit: '編集',
+  },
+};
+
+export default lang;
