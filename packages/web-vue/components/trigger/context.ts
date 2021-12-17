edd74c4d8a5854f45606da603edb999f9860c8f5
wangchen <wangchen.flsion@bytedance.com>
2021-12-17 14:07:57 +0800

    fix(trigger): fix resize & context problem

--- packages/web-vue/components/trigger/context.ts
+++ packages/web-vue/components/trigger/context.ts
@@ -3,8 +3,6 @@ import type { InjectionKey } from 'vue';
 export interface TriggerContext {
   onMouseenter: (ev: MouseEvent) => void;
   onMouseleave: (ev: MouseEvent) => void;
-  onFocusin: (ev: FocusEvent) => void;
-  onFocusout: (ev: FocusEvent) => void;
   addChildRef: (ref: any) => void;
   removeChildRef: (ref: any) => void;
 }
