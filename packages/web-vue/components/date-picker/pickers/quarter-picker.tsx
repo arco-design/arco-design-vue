a7459f535cf772929e2fdc33cafc6d21af71df72
kira <kirazxyun@gmail.com>
2022-05-13 17:42:33 +0800

    fix(date-picker): week and quarter has a wrong value (#1112)

--- packages/web-vue/components/date-picker/pickers/quarter-picker.tsx
+++ packages/web-vue/components/date-picker/pickers/quarter-picker.tsx
@@ -27,6 +27,15 @@ export default defineComponent({
       type: String,
       default: 'YYYY-[Q]Q',
     },
+    /**
+     * @zh 值的格式，对 `value` `defaultValue` `pickerValue` `defaultPickerValue` 以及事件中的返回值生效，支持设置为时间戳，Date 和字符串（参考[字符串解析格式](#字符串解析格式)）。
+     * @en The format of the value, valid for `value` `defaultValue` `pickerValue` `defaultPickerValue` and the return value in the event, supports setting as timestamp, Date and string (refer to [String parsing format](#string-parsing-format) ).
+     * @version 2.16.0
+     */
+    valueFormat: {
+      type: String,
+      default: 'YYYY-MM',
+    },
   },
   setup(props: QuarterPickerProps, { attrs, slots }) {
     return () => (
