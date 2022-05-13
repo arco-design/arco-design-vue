f8f8486d962a769d72502f6dca906dc9036ec510
wangchen <wangchen.flsion@bytedance.com>
2022-05-13 15:13:27 +0800

    feat: update commit config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'enhance',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
