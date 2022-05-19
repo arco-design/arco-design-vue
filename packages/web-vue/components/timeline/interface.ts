ee14f1afd68f512052e0f1420a6ee4aa37e0993d
wangchen <wangchen.flsion@bytedance.com>
2022-05-19 17:12:11 +0800

    enhance(timeline): ARIA improvement

---
+++ packages/web-vue/components/timeline/interface.ts
@@ -0,0 +1,9 @@
+export type ModeType = 'left' | 'right' | 'top' | 'bottom' | 'alternate';
+
+export type LabelPositionType = 'relative' | 'same';
+
+export type DotType = 'hollow' | 'solid';
+
+export type LineType = 'solid' | 'dashed' | 'dotted';
+
+export type PositionType = 'left' | 'right' | 'top' | 'bottom';
