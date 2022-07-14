ac82c161c9fb1f96e0d4c6e1ab9e45b5f070e34c
番薯有才 <tazytan@qq.com>
2022-07-14 11:50:46 +0800

    feat(drawer): add function call

--- packages/web-vue/components/drawer/index.ts
+++ packages/web-vue/components/drawer/index.ts
@@ -1,16 +1,112 @@
-import type { App } from 'vue';
-import type { ArcoOptions } from '../_utils/types';
-import { setGlobalConfig, getComponentPrefix } from '../_utils/global-config';
+import type { App, AppContext } from 'vue';
+import { createVNode, render } from 'vue';
+import { getOverlay } from '../_utils/dom';
+import { getComponentPrefix, setGlobalConfig } from '../_utils/global-config';
+import { isFunction } from '../_utils/is';
+import { omit } from '../_utils/omit';
+import { ArcoOptions } from '../_utils/types';
+import { getSlotFunction } from '../_utils/vue-utils';
 import _Drawer from './drawer.vue';
+import { DrawerConfig, DrawerMethod } from './interface';
+
+const open = (config: DrawerConfig, appContext?: AppContext) => {
+  let container: HTMLElement | null = getOverlay('drawer');
+
+  const handleOk = () => {
+    if (vm.component) {
+      vm.component.props.visible = false;
+    }
+
+    if (isFunction(config.onOk)) {
+      config.onOk();
+    }
+  };
+
+  const handleCancel = () => {
+    if (vm.component) {
+      vm.component.props.visible = false;
+    }
+
+    if (isFunction(config.onCancel)) {
+      config.onCancel();
+    }
+  };
+
+  const handleClose = () => {
+    if (container) {
+      render(null, container);
+      document.body.removeChild(container);
+    }
+    container = null;
+
+    if (isFunction(config.onClose)) {
+      config.onClose();
+    }
+  };
+
+  const defaultConfig = {
+    visible: true,
+    renderToBody: false,
+    onOk: handleOk,
+    onCancel: handleCancel,
+    onClose: handleClose,
+  };
+
+  // @ts-ignore
+  const vm = createVNode(
+    _Drawer,
+    {
+      ...omit(config, ['content', 'title', 'footer']),
+      ...{
+        header: typeof config.header === 'boolean' ? config.header : undefined,
+        footer: typeof config.footer === 'boolean' ? config.footer : undefined,
+      },
+      ...defaultConfig,
+    },
+    {
+      default: getSlotFunction(config.content),
+      header:
+        typeof config.header !== 'boolean'
+          ? getSlotFunction(config.header)
+          : undefined,
+      title: getSlotFunction(config.title),
+      footer:
+        typeof config.footer !== 'boolean'
+          ? getSlotFunction(config.footer)
+          : undefined,
+    }
+  );
+
+  if (appContext ?? Drawer._context) {
+    vm.appContext = appContext ?? Drawer._context;
+  }
+
+  render(vm, container);
+  document.body.appendChild(container);
+
+  return {
+    close: handleClose,
+  };
+};
 
 const Drawer = Object.assign(_Drawer, {
+  open,
   install: (app: App, options?: ArcoOptions) => {
     setGlobalConfig(app, options);
     const componentPrefix = getComponentPrefix(options);
 
     app.component(componentPrefix + _Drawer.name, _Drawer);
+
+    const drawerWithContext: DrawerMethod = {
+      open: (config, appContext = app._context) => open(config, appContext),
+    };
+
+    app.config.globalProperties.$drawer = drawerWithContext;
   },
+  _context: null as AppContext | null,
 });
+
+export type { DrawerMethod, DrawerConfig, DrawerReturn } from './interface';
 
 export type DrawerInstance = InstanceType<typeof _Drawer>;
 
