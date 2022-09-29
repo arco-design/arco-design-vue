12682814133eb9e4f681aca6f50aab9c8e2eab2c
guochao <463561248@qq.com>
2022-09-29 16:00:17 +0800

    enhance(modal): Model.open can set renderToBody

--- packages/web-vue/components/modal/index.tsx
+++ packages/web-vue/components/modal/index.tsx
@@ -57,11 +57,19 @@ const open = (config: ModalConfig, appContext?: AppContext) => {
   const vm = createVNode(
     _Modal,
     {
-      ...omit(config, ['content', 'title', 'footer']),
+      ...defaultConfig,
+      ...omit(config, [
+        'content',
+        'title',
+        'footer',
+        'visible',
+        'onOk',
+        'onCancel',
+        'onClose',
+      ]),
       ...{
         footer: typeof config.footer === 'boolean' ? config.footer : undefined,
       },
-      ...defaultConfig,
     },
     {
       default: getSlotFunction(config.content),
