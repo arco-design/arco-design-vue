04e479e0cd2f7d5b1b72f337d549ecce7213d9ee
wangchen <wangchen.flsion@bytedance.com>
2022-10-28 19:27:04 +0800

    fix: update dependencies

--- packages/arco-vue-docs-navbar/src/index.tsx
+++ packages/arco-vue-docs-navbar/src/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // @ts-ignore
 import ReactDOM from 'react-dom';
-import Navbar from '@arco-materials/site-navbar';
+import Navbar from '@arco-materials/site-navbar-new';
 import { ConfigProvider } from '@arco-design/web-react';
 import './index.less';
 
