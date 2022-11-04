f1e84ea29df60a75237db9cecabfc96af3563195
番薯有才 <tazytan@qq.com>
2022-11-04 17:29:33 +0800

    fix(table): fix param when rowClass as function (#1812)

--- packages/web-vue/components/table/table.tsx
+++ packages/web-vue/components/table/table.tsx
@@ -1461,7 +1461,7 @@ export default defineComponent({
           class={[
             `${prefixCls}-tr-summary`,
             isFunction(props.rowClass)
-              ? props.rowClass(record, rowIndex)
+              ? props.rowClass(record.raw, rowIndex)
               : props.rowClass,
           ]}
           // @ts-ignore
