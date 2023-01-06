e24e481be202477ee79fbc8430759008201338f9
Flsion <wangchen.flsion@bytedance.com>
2023-01-06 19:21:14 +0800

    fix: fix typescript problem (#2027)

--- packages/web-vue/components/components.ts
+++ packages/web-vue/components/components.ts
@@ -4,7 +4,7 @@ import { NotificationMethod } from './notification';
 import { ModalMethod } from './modal';
 import { DrawerMethod } from './drawer';
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AAffix: typeof import('@arco-design/web-vue')['Affix'];
     AAlert: typeof import('@arco-design/web-vue')['Alert'];
