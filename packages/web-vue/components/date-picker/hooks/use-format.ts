d4758ec025a9424349db9aef30c02a9ef7667cdc
guochao <463561248@qq.com>
2023-05-20 15:44:05 +0800

    fix(date-picker): add getDefaultValueFormat to solve v-model for week selector and quarter selector

--- packages/web-vue/components/date-picker/hooks/use-format.ts
+++ packages/web-vue/components/date-picker/hooks/use-format.ts
@@ -26,6 +26,23 @@ export function getDefaultFormat(mode: Mode = 'date', showTime = false) {
   }
 }
 
+export function getDefaultValueFormat(mode: Mode = 'date', showTime = false) {
+  switch (mode) {
+    case 'date':
+      return showTime ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD';
+    case 'month':
+      return 'YYYY-MM';
+    case 'year':
+      return 'YYYY';
+    case 'week':
+      return 'YYYY-MM-DD';
+    case 'quarter':
+      return 'YYYY-MM';
+    default:
+      return 'YYYY-MM-DD';
+  }
+}
+
 export default function useFormat(props: FormatProps) {
   const {
     format: propFormat,
@@ -40,7 +57,11 @@ export default function useFormat(props: FormatProps) {
       getDefaultFormat(mode?.value, showTime?.value)
   );
 
-  const valueFormat = computed(() => propValueFormat?.value || format.value);
+  const valueFormat = computed(
+    () =>
+      propValueFormat?.value ||
+      getDefaultValueFormat(mode?.value, showTime?.value)
+  );
 
   const parseValueFormat = computed(() =>
     ['timestamp', 'Date'].includes(valueFormat.value)
