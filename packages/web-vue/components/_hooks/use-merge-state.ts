7f722125b66a0749fad1039799ce7408929788ef
詹大帅 <zhanyangqi@163.com>
2023-07-29 16:35:43 +0800

    Update use-merge-state.ts
    useSetState

--- packages/web-vue/components/_hooks/use-merge-state.ts
+++ packages/web-vue/components/_hooks/use-merge-state.ts
@@ -1,13 +1,13 @@
 import { Ref, toRefs, computed, watch, ComputedRef } from 'vue';
 import { isUndefined } from '../_utils/is';
-import useState from './use-state';
+import useSetState from './use-state';
 
 export default function useMergeState<T, E = T | undefined>(
   defaultValue: T,
   props: { value: E }
 ): [ComputedRef<T>, (val: E) => void, Ref<T>] {
   const { value } = toRefs(props);
-  const [localValue, setLocalValue] = useState(
+  const [localValue, setLocalValue] = useSetState(
     !isUndefined(value.value) ? value.value : defaultValue
   );
   watch(value, (newVal) => {
