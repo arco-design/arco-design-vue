43109c357241692f4717e7f25c0617b39e661cf1
seepine <93666053+seepine@users.noreply.github.com>
2023-08-12 04:48:31 +0000

    fix(form-item): fix error message not display when help set

--- packages/web-vue/components/form/form-item-message.vue
+++ packages/web-vue/components/form/form-item-message.vue
@@ -1,26 +1,33 @@
 <template>
-  <transition v-if="help || $slots.help" name="form-blink" appear>
+  <template v-if="error.length > 0">
+    <template v-for="item of error" :key="item">
+      <transition name="form-blink" appear>
+        <div role="alert" :class="[prefixCls]">
+          {{ item }}
+        </div>
+      </transition>
+    </template>
+  </template>
+  <transition v-else-if="help || $slots.help" name="form-blink" appear>
     <div :class="[prefixCls, `${prefixCls}-help`]">
       <slot name="help">
         {{ help }}
       </slot>
     </div>
   </transition>
-  <transition v-for="item of error" v-else :key="item" name="form-blink" appear>
-    <div role="alert" :class="[prefixCls]">
-      {{ item }}
-    </div>
-  </transition>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import { getPrefixCls } from '../_utils/global-config';
 
 export default defineComponent({
   name: 'FormItemMessage',
   props: {
-    error: Array,
+    error: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
     help: String,
   },
   setup() {
