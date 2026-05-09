cc93739613b75e87d8e7eb4d7a9e81564ed57af6
Norris Lau <liunian@2077tech.com>
2026-05-09 10:54:58 +0800

    Add App.vue

---
+++ packages/web-vue-debug/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { shallowRef } from 'vue';
+
+  import Select from '@web-vue-src/select';
+
+  const options = shallowRef(
+    Array.from({ length: 100 }, (_, i) => ({
+      label: `选项${i + 1}长长长长长长长长长`,
+      value: i + 1,
+    })),
+  );
+</script>
+
+<template>
+  <main style="width: 200px">
+    <Select :options="options" multiple max-tag-count="responsive" />
+  </main>
+</template>
